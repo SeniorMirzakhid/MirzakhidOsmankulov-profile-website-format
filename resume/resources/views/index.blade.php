b237f73fd8f7b3967c8647c6efdb11258aaac4a4
Mirzakhid Osmankulov <mirzakhidosmankulov@gmail.com>
2021-03-17 12:01:17 +0600

    Changing information

--- resume/resources/views/index.blade.php
+++ resume/resources/views/index.blade.php
@@ -2,8 +2,19 @@
 <html>
 <head>
 	<title>This is my website for CV</title>
+	
 </head>
-<body>
+<body style="margin: 0;">
+<div class="first" style="display: flex; justify-content: space-around; padding: 30px;  font-size: 30px; background-color: yellow;margin: 0px;">
+	<div class="main">About me</div>
+	<div class="main">Skills</div>
+	<div class="main">Contacts</div>
+</div>
+<div class="navbar">
 
+</div>
+<div class="footer">
+	
+</div>
 </body>
 </html>
