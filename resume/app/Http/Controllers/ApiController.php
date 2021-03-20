03ecb807108aa54c420f15fda7a657419403671d
Mirzakhid Osmankulov <mirzakhidosmankulov@gmail.com>
2021-03-20 13:00:50 +0600

    changing files

---
+++ resume/app/Http/Controllers/ApiController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Client;
+class ApiController extends Controller
+{
+    public function index(Request $request){
+$clients = Client::all();
+
+return response($clients, 200);
+    }
+
+    public function get_client(Request $request){
+    	$client = Client::find($request->client_id);
+    	if ($client == null){
+    		return response(['message' => 'There is no client'], 404);
+
+
+    	}
+
+    	return response($client, 200);
+    }
+}
--- resume/routes/api.php
+++ resume/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/clients', [ApiController::class, 'index']);
+
+Route::get('/clients/details', [ApiController::class, 'get_client']);
